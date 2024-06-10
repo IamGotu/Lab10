7020010a5f73b3b93d5cd0da1c162f3e6955b8ae
Mark John <markjohnjopia1@gmail.com>
2024-06-10 18:01:25 +0800

    will update the active = Offline if user logged out

--- logout.php
+++ logout.php
@@ -1,6 +1,17 @@
 <?php
 session_start();
 
+include ('database/db_conn.php');
+
+if (isset($_SESSION['email'])) {
+    // Update user status to 'Offline'
+    $email = $_SESSION['email'];
+    $update_status_query = "UPDATE users SET active = 'Offline' WHERE email = ?";
+    $update_status_stmt = $conn->prepare($update_status_query);
+    $update_status_stmt->bind_param("s", $email);
+    $update_status_stmt->execute();
+}
+
 // Unset all session variables
 session_unset();
 
