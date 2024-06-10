76118200d43591c0e64fce9faca577b923718a26
Mark John <markjohnjopia1@gmail.com>
2024-06-10 18:02:18 +0800

    updating session

--- verification_process.php
+++ verification_process.php
@@ -25,24 +25,24 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $update_sql = "UPDATE users SET Status='Verified' WHERE Email='$email' AND verify_token='$verification_code'";
         if (mysqli_query($conn, $update_sql)) {
             // Redirect the user to a page indicating successful verification
-            $_SESSION['status'] = "You have successfully verified. You can now login your account";
+            $_SESSION['auth_status'] = "You have successfully verified. You can now login your account";
             header("Location: Loginform.php?success=you have successfully verified");
             exit();
         } else {
             // Display an error message if the update fails
-            $_SESSION['status'] = "Verification failed, please try again";
+            $_SESSION['auth_status'] = "Verification failed, please try again";
             header("Location: VerifyEmail.php?email=$email&error=Verification failed, please try again");
             exit();
         }
     } else {
         // Redirect the user to a page indicating invalid verification code
-        $_SESSION['status'] = "Invalid verification code, please try again";
+        $_SESSION['auth_status'] = "Invalid verification code, please try again";
         header("Location: VerifyEmail.php?email=$email&error=Invalid verification code, please try again");
         exit();
     }
 } else {
     // Redirect the user if they try to access this page directly
-    $_SESSION['status'] = "You cannot access this site directly";
+    $_SESSION['auth_status'] = "You cannot access this site directly";
     header("Location: Loginform.php?error=You cannot access this site directly");
     exit();
 }
