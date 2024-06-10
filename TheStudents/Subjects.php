367dbfc99c784adfdf93d4cf431203e71ece99b9
Mark John <markjohnjopia1@gmail.com>
2024-06-10 19:02:04 +0800

    fixing errors in Subjects.php, adding restriction, and removing code that is not use

--- TheStudents/Subjects.php
+++ TheStudents/Subjects.php
@@ -2,9 +2,19 @@
 session_start();
 
 // Check if user is not logged in
-if (!isset($_SESSION['auth'])) {
-    $_SESSION['auth_status'] = "You need to be logged in to access this page";
-    header('Location: ../loginform.php');
+if (!isset($_SESSION['auth']) || $_SESSION['auth'] !== true) {
+  $_SESSION['auth_status'] = "You need to be logged in to access this page";
+  header('Location: ../loginform.php');
+  exit();
+}
+
+// Specificaly admin access only
+$required_role = 'student';
+
+// Check if the user has the required role
+if ($_SESSION['role'] !== $required_role) {
+    $_SESSION['auth_status'] = "You do not have permission to access this page";
+    header('Location: Dashboard.php');
     exit();
 }
 
@@ -18,81 +28,7 @@ include('sidebar.php');
 <!-- Content Wrapper. Contains page content -->
 <div class="content-wrapper">
 
-<!-- Modal -->
-<div class="modal fade" id="usersModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h1 class="modal-title fs-5" id="exampleModalLabel">Add User</h1>
-        <button type="button" class="btn-close" data-dismiss="modal" aria-label="Close"></button>
-      </div>
-      <form action="code.php" method="post">
-      <div class="modal-body">
-        <div class="form-group">
-          <label for="name">Name</label>
-          <input type="text" id="name" name="name" class="form-control" placeholder="Name" >
-        </div>
-        <div class="form-group">
-          <label for="email">Email</label>
-          <input type="email" id="email" name="email" class="form-control" placeholder="Email" >
-        </div>
-        <div class="form-group">
-          <label for="phone">PhoneNo.</label>
-          <input type="text" id="phone" name="phone" class="form-control" placeholder="PhoneNo." >
-        </div>
-        <div class="form-group">
-          <label for="address">Address</label>
-          <input type="text" id="address" name="address" class="form-control" placeholder="Address" >
-        </div>
-        <div class="row">
-          <div class="col-md-6">
-            <div class="form-group">
-              <label for="pass">Password</label>
-              <input type="password" id="pass" name="pass" class="form-control" placeholder="Password" required>
-            </div>
-          </div>
-          <div class="col-md-6">
-            <div class="form-group">
-              <label for="confirm_pass">Confirm Password</label>
-              <input type="password" id="confirm_pass" name="confirm_pass" class="form-control" placeholder="Confirm Password" required>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-        <button type="submit" name="addUser" class="btn btn-primary">Save</button>
-      </div>
-      </form>
-    </div>
-  </div>
-</div>
-
-<!-- Delete User Modal -->
-<div class="modal fade" id="DeleteModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h1 class="modal-title fs-5" id="exampleModalLabel">Delete User</h1>
-        <button type="button" class="btn-close" data-dismiss="modal" aria-label="Close"></button>
-      </div>
-      <form action="code.php" method="post">
-      <div class="modal-body">
-        <input type="hidden" name="delete_id" class="delete_user_id" >
-        <p>
-          Are you sure. you want to delete this data?
-        </p>
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-        <button type="submit" name="deleteUserbtn" class="btn btn-primary">Yes, Delete.!</button>
-      </div>
-      </form>
-    </div>
-  </div>
-</div>
-
-        <!-- Content Header (Page header) -->
+    <!-- Content Header (Page header) -->
     <div class="content-header">
       <div class="container-fluid">
         <div class="row mb-2">
@@ -109,6 +45,10 @@ include('sidebar.php');
     </div><!-- /.container-fluid -->
 </div>
 <!-- /.content-header -->
+
+<?php
+  include ('../includes/message.php');
+?>
 
     <!-- Main content -->
 <section class="content">
