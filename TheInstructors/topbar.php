48b5b0777fa69a2a7f30156214aaaf94efc47b2a
Mark John <markjohnjopia1@gmail.com>
2024-06-11 01:10:49 +0800

    changing folder name

--- TheInstructors/topbar.php
+++ TheInstructors/topbar.php
@@ -1,3 +1,13 @@
+<?php
+// Check if user is not logged in
+if (!isset($_SESSION['auth']) || $_SESSION['auth'] !== true) {
+    $_SESSION['auth_status'] = "You need to be logged in to access this page";
+    header('Location: ../loginform.php');
+    exit();
+}
+
+$user_details = $_SESSION['user_details'];
+?>
 <!-- Navbar -->
 <nav class="main-header navbar navbar-expand navbar-white navbar-light">
   <!-- Left navbar links -->
@@ -6,29 +16,27 @@
       <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
     </li>
     <li class="nav-item d-none d-sm-inline-block">
-      <a href="../TheTeachers/teachers_Home.php" class="nav-link">Home</a>
+      <a href="../TheStudents/Dashboard.php" class="nav-link">Home</a>
     </li>
     <li class="nav-item d-none d-sm-inline-block">
-      <a href="../TheTeachers/contact.php" class="nav-link">Contact</a>
+      <a href="../TheStudents/contact.php" class="nav-link">Contact</a>
     </li>
   </ul>
 
   <!-- Right navbar links -->
   <ul class="navbar-nav ml-auto">
     <li class="nav-item">
-      <?php if (isset($_SESSION['auth'])) { ?>
         <div class="dropdown">
           <button class="btn btn-secondary dropdown-toggle" type="button" data-toggle="dropdown" aria-expanded="false">
-            <?php echo $_SESSION['auth_user']['full_name']; ?>
+            <?php echo $user_details['full_name']; ?>
           </button>
           <ul class="dropdown-menu">
+            <li><a href="../TheAdmin/User_Profile.php" class="dropdown-item">User Profile</a></li>
             <li><a href="../logout.php" class="dropdown-item">Logout</a></li>
           </ul>
         </div>
-      <?php } else { ?>
-        <a href="loginform.php" class="nav-link">Login</a>
-      <?php } ?>
     </li>
+    
     <!-- Navbar Search -->
     <li class="nav-item">
       <a class="nav-link" data-widget="navbar-search" href="#" role="button">
