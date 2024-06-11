f9abc97a1e4102ff7ec87a8049cd54274501c52b
Mark John <markjohnjopia1@gmail.com>
2024-06-11 10:35:55 +0800

    fixing students file to display students data from database

--- TheInstructors/Students.php
+++ TheInstructors/Students.php
@@ -2,9 +2,19 @@
 session_start();
 
 // Check if user is not logged in
-if (!isset($_SESSION['auth'])) {
-    $_SESSION['auth_status'] = "You need to be logged in to access this page";
-    header('Location: loginform.php');
+if (!isset($_SESSION['auth']) || $_SESSION['auth'] !== true) {
+  $_SESSION['auth_status'] = "You need to be logged in to access this page";
+  header('Location: ../loginform.php');
+  exit();
+}
+
+// Specificaly instructor access only
+$required_role = 'instructor';
+
+// Check if the user has the required role
+if ($_SESSION['role'] !== $required_role) {
+    $_SESSION['auth_status'] = "You do not have permission to access this page";
+    header('Location: Dashboard.php');
     exit();
 }
 
@@ -22,18 +32,22 @@ include('sidebar.php');
   <div class="container-fluid">
     <div class="row mb-2">
       <div class="col-sm-6">
-        <h1 class="m-0">Dashboard</h1>
+        <h1 class="m-0">Students</h1>
       </div><!-- /.col -->
       <div class="col-sm-6">
         <ol class="breadcrumb float-sm-right">
           <li class="breadcrumb-item"><a href="Dashboard.php">Home</a></li>
-          <li class="breadcrumb-item active">Enrolled Students</li>
+          <li class="breadcrumb-item active">Students</li>
         </ol>
       </div><!-- /.col -->
     </div><!-- /.row -->
   </div><!-- /.container-fluid -->
 </div>
 <!-- /.content-header -->
+
+<?php
+  include ('../includes/message.php');
+?>
 
 <!-- Main content -->
 <section class="content">
@@ -48,25 +62,29 @@ include('sidebar.php');
         ?>
         <div class="card">
           <div class="card-header">
-            <h3 class="card-title">Enrolled Students</h3>
+            <h3 class="card-title">Students</h3>
           </div>
           <!-- /.card-header -->
           <div class="card-body">
             <table id="example2" class="table table-bordered table-hover">
               <thead>
                 <tr>
-                  <th>ID</th>
+                <th>Student ID</th>
                   <th>Name</th>
-                  <th>Email</th>
-                  <th>Address</th>
+                  <th>Birthdate</th>
                   <th>Age</th>
                   <th>Gender</th>
-                  <th>Course</th>
+                  <th>Email</th>
+                  <th>Phone Number</th>
+                  <th>Address</th>
                 </tr>
               </thead>
               <tbody>
                 <?php 
-                $query = "SELECT * FROM student_list";
+                $query = "SELECT students.*, users.user_id
+                          FROM students JOIN users
+                          ON students.student_id = users.user_id";
+
                 $run_query = mysqli_query($conn, $query);
                 
                 if (!$run_query) {
@@ -75,15 +93,16 @@ include('sidebar.php');
                 
                 if(mysqli_num_rows($run_query) > 0) {
                   while ($row = mysqli_fetch_assoc($run_query)) {
-                    ?>
+              ?>
                     <tr>
                       <td><?php echo $row['student_id'] ?></td>
                       <td><?php echo $row['full_name'] ?></td>
-                      <td><?php echo $row['email'] ?></td>
-                      <td><?php echo $row['address'] ?></td>
+                      <td><?php echo $row['birthdate'] ?></td>
                       <td><?php echo $row['age'] ?></td>
                       <td><?php echo $row['gender'] ?></td>
-                      <td><?php echo $row['course'] ?></td>
+                      <td><?php echo $row['email'] ?></td>
+                      <td><?php echo $row['phone_number'] ?></td>
+                      <td><?php echo $row['address'] ?></td>
                     </tr>
                     <?php
                   }
